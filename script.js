d10c2bca84ba277db795324eac57dd32a8c2d1cf
chris-base <baseappsdev@gmail.com>
2022-06-06 20:01:44 -0500

    used json to update graph values

--- script.js
+++ script.js
@@ -1,19 +1,24 @@
-let dayStrings = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+fetch("data.json")
+  .then((response) => response.json())
+  .then((json) => {
+    // console.log(json.length);
 
-for (let i = 0; i < dayStrings.length; i++) {
-  document.getElementById(`${dayStrings[i]}GraphContainer`).addEventListener("mouseover", function () {
-    document.getElementById(`${dayStrings[i]}HoverContainer`).style.opacity = 1;
-  });
+    for (let i = 0; i < json.length; i++) {
+      // add hover opacity to bars in graph
+      document.getElementById(`${json[i].day.toLowerCase()}GraphContainer`).addEventListener("mouseover", function () {
+        document.getElementById(`${json[i].day.toLowerCase()}HoverContainer`).style.opacity = 1;
+      });
+      document.getElementById(`${json[i].day.toLowerCase()}GraphContainer`).addEventListener("mouseleave", function () {
+        document.getElementById(`${json[i].day.toLowerCase()}HoverContainer`).style.opacity = 0;
+      });
+      document.getElementById(`${json[i].day.toLowerCase()}GraphContainer`).addEventListener("mouseover", function () {
+        document.getElementById(`${json[i].day.toLowerCase()}GraphContainer`).style.opacity = 0.6;
+      });
+      document.getElementById(`${json[i].day.toLowerCase()}GraphContainer`).addEventListener("mouseleave", function () {
+        document.getElementById(`${json[i].day.toLowerCase()}GraphContainer`).style.opacity = 1;
+      });
 
-  document.getElementById(`${dayStrings[i]}GraphContainer`).addEventListener("mouseleave", function () {
-    document.getElementById(`${dayStrings[i]}HoverContainer`).style.opacity = 0;
+      // add hover dollar values to divs
+      document.getElementById(`${json[i].day.toLowerCase()}HoverText`).innerText = "$" + json[i].amount;
+    }
   });
-
-  document.getElementById(`${dayStrings[i]}GraphContainer`).addEventListener("mouseover", function () {
-    document.getElementById(`${dayStrings[i]}GraphContainer`).style.opacity = 0.6;
-  });
-
-  document.getElementById(`${dayStrings[i]}GraphContainer`).addEventListener("mouseleave", function () {
-    document.getElementById(`${dayStrings[i]}GraphContainer`).style.opacity = 1;
-  });
-}
